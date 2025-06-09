fad74e37089f4173d5d945868cc5316846d305ed
Drew Berry <drewberry612@outlook.com>
2025-06-09 12:39:50 +0100

    Upload app update

--- backend/config.py
+++ backend/config.py
@@ -4,8 +4,8 @@ from pathlib import Path
 class Settings(BaseSettings):
     LOG_DIR: Path = Path("logs/")
     LOG_UPLOAD_INTERVAL_MINS: int = 30  # Interval to upload logs
-    GOOGLE_DRIVE_FOLDER_ID: str  # Must be set via environment or .env file
-    CREDENTIALS_PATH: Path = Path("credentials.json")  # Google service account creds
+    #GOOGLE_DRIVE_FOLDER_ID: str  # Must be set via environment or .env file
+    #CREDENTIALS_PATH: Path = Path("credentials.json")  # Google service account creds
 
     class Config:
         env_file = ".env"
